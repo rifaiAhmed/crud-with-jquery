aab809421e0869f68c8a2e652f4343a08d2d5a4a
rifaiAhmed <rifaiahmed078@gmail.com>
2022-12-08 06:09:34 +0700

    1

---
+++ perpustakaan.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 08, 2022 at 12:08 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_buku`
+--
+
+CREATE TABLE `tb_buku` (
+  `id` int(11) NOT NULL,
+  `judul_buku` varchar(25) NOT NULL,
+  `pengarang` varchar(25) NOT NULL,
+  `tahun_terbit` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_buku`
+--
+
+INSERT INTO `tb_buku` (`id`, `judul_buku`, `pengarang`, `tahun_terbit`) VALUES
+(3, 'DBMS', 'Ahmed', '2030');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_buku`
+--
+ALTER TABLE `tb_buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
